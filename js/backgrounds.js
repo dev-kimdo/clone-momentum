65a5cf747af158ddc4bde6a0a8ab9401a82a4803
dev-kimdo <dev.kimdo@gmail.com>
2022-04-11 15:52:41 +0900

    [clone] momentum

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -1,7 +1,14 @@
 const images = [
-    "1.jpeg",
-    "2.jpeg",
-    "3.jpeg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
--- js/weather.js
+++ js/weather.js
@@ -6,14 +6,14 @@ const API_KEY = "c038d754ea947cb3d90c2aa9975fb431";
 function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`
     fetch(url)
     .then(response => response.json())
     .then(data => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        weather.innerText = `${data.weather[0].main}/ ${data.main.temp}℃ `;
     });
 }
 
